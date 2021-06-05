5d5500491072f02eb67f89e56d08df97cc902f96
Ariel <Ariel@DESKTOP-FK39BEL>
2021-06-05 16:23:30 -0300

    inicio

---
+++ test/reservas/ReservaTestCase.java
@@ -0,0 +1,19 @@
+package reservas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ReservaTestCase {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
