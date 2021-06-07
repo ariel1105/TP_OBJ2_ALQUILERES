b7c527d36ec00ecfb4cdb1249cfd17d5112b248f
Lautaro <Lautaro@DESKTOP-B1AU2OU>
2021-06-07 13:07:52 -0300

    Se agregaron las clases Publicacion, Busqueda, Categoria, PoliticaDeCancelacion y un testCase de busqueda, falta completar el metodo busqueda

---
+++ src/reservas/Publicacion.java
@@ -0,0 +1,47 @@
+package reservas;
+
+import java.util.List;
+
+public class Publicacion {
+	
+	private Inmueble inmuebleAsignado;
+	private	List<String> comentarios;
+	private List<Categoria> categorias;
+	private Integer vecesAlquilado;
+	private PoliticaDeCancelacion politicaDeCancelacion;
+	
+	public Publicacion(Inmueble inmueble) {
+		
+		this.setInmuebleAsignado(inmueble);
+		
+		
+	}
+
+	public Inmueble getInmuebleAsignado() {
+		return inmuebleAsignado;
+	}
+
+	public void setInmuebleAsignado(Inmueble inmuebleAsignado) {
+		this.inmuebleAsignado = inmuebleAsignado;
+	}
+
+	public List<String> getComentarios() {
+		return comentarios;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public Integer getVecesAlquilado() {
+		return vecesAlquilado;
+	}
+
+	public PoliticaDeCancelacion getPoliticaDeCancelacion() {
+		return politicaDeCancelacion;
+	}
+	
+	
+
+
+}
